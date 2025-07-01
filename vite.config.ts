43315d252026148059886735d5e9c6db29038732
ShiritoDev <mapi27022005@gmail.com>
2025-07-01 10:10:55 -0400

    Configurar Netlify para resolver problemas de MIME type

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  },
+  base: './'
 })
